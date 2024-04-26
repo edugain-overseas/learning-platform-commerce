95e62880311cc13cde93c47393b18824e8753433
Roman Polishchuk <polrmn@MacBook-Air-MacBook.local>
2024-04-26 11:47:40 +0300

    server update

--- src/http/sever.js
+++ src/http/sever.js
@@ -1,3 +1,3 @@
-export const serverBaseUrl = "https://a19a2089e365.ngrok.app/api/v1";
-export const serverName = "https://a19a2089e365.ngrok.app";
-export const webSocketUrl = "wss://a19a2089e365.ngrok.app/api/v1/chat";
+export const serverBaseUrl = "https://56905671d38f.ngrok.app/api/v1";
+export const serverName = "https://56905671d38f.ngrok.app";
+export const webSocketUrl = "wss://56905671d38f.ngrok.app/api/v1/chat";
