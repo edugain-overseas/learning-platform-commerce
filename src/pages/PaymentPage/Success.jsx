4a120d92ebba97ebffd1a63fc9cf95230cf14545
Roman Polishchuk <polrmn@MacBook-Air-MacBook.local>
2025-07-16 17:07:16 +0300

    feature/payment page/sucess animation

--- src/pages/PaymentPage/Success.jsx
+++ src/pages/PaymentPage/Success.jsx
@@ -1,14 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import SuccessImg from "../../images/Success.webp";
 import { ReactComponent as Arrow } from "../../images/icons/arrow-left.svg";
+import { ReactComponent as Like } from "../../images/icons/like.svg";
 import styles from "./PaymentPage.module.scss";
 
 const Success = () => {
   return (
     <div className={styles.successWrapper}>
       <div className={styles.topContentWrapper}>
-        <img src={SuccessImg} alt="success"/>
+        {/* <img src={SuccessImg} alt="success" /> */}
+        <div className={styles.circlesWrapper}>
+          <div className={styles.circleOuter}>
+            <div className={styles.circleMiddle}>
+              <div className={styles.circleInner}></div>
+            </div>
+          </div>
+          <div className={styles.contentInner}></div>
+          <div className={styles.like}>
+            <Like />
+          </div>
+        </div>
+
         <p>Your purchase</p>
         <p>Was successful!</p>
       </div>
