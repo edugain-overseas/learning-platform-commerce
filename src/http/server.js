2839408cf8047b9605fd1dfe3ca396243696b9fd
Roman Polishchuk <polrmn@MacBook-Air-MacBook.local>
2025-08-21 15:29:36 +0300

    fix/server url change

--- src/http/server.js
+++ src/http/server.js
@@ -1,6 +1,3 @@
-// export const serverBaseUrl = "https://de4b-91-123-154-50.ngrok-free.app/api/v1";
-// export const serverName = "https://de4b-91-123-154-50.ngrok-free.app";
-// export const webSocketUrl = "wss://de4b-91-123-154-50.ngrok-free.app/api/v1/chat";
-export const serverBaseUrl = "https://vps2.xyz/api/v1";
-export const serverName = "https://vps2.xyz/api";
-export const webSocketUrl = "wss://vps2.xyz/api/v1/chat";
+export const serverBaseUrl = "https://vps2.online/api/v1";
+export const serverName = "https://vps2.online/api";
+export const webSocketUrl = "wss://vps2.online/api/v1/chat";
