6190d95ffff21ec66b1e9f0556b141fa6962c8cb
Roman Polishchuk <polrmn@MacBook-Air-MacBook.local>
2026-01-14 10:41:11 +0200

    feature/contact form error notification

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./ContactForm.module.scss";
 import Textarea from "../shared/Textarea/Textarea";
 import { useForm } from "react-hook-form";
+import { useNotificationMessage } from "../../hooks/useNotificationMessage";
 
 const requiredRegisterArgs = {
   required: {
@@ -13,53 +14,67 @@ const requiredRegisterArgs = {
 const ContactForm = ({ wrapperClassname = "" }) => {
   const { register, handleSubmit, formState } = useForm();
   const { errors } = formState;
+  const [messageApi, contextHolder] = useNotificationMessage();
 
   const onSubmit = (data) => {
     console.log(data);
   };
 
+  const onError = (errors) => {
+    console.log(errors);
+    const fields = Object.keys(errors);
+
+    messageApi.error({
+      duration: 3,
+      content: `Fields ${fields.join(", ")} is required`,
+    });
+  };
+
   return (
-    <form
-      className={`${styles.formWrapper} ${wrapperClassname}`}
-      onSubmit={handleSubmit(onSubmit)}
-    >
-      <div className={styles.formHeader}>
-        <span className={styles.title}>Contact us</span>
-        <span className={styles.secondaryTitle}>
-          Still have more questions? Would be glad to assist.
-        </span>
-      </div>
-      <div className={styles.fields}>
-        <label className={errors.name ? styles.withError : ""}>
-          <span>Name</span>
-          <input type="text" {...register("name", requiredRegisterArgs)} />
-          {errors.name && (
-            <span className={styles.error}>{errors.name.message}</span>
-          )}
-        </label>
-        <label className={errors.email ? styles.withError : ""}>
-          <span>Email</span>
-          <input type="email" {...register("email", requiredRegisterArgs)} />
-          {errors.email && (
-            <span className={styles.error}>{errors.email.message}</span>
-          )}
-        </label>
-        <label className={errors.phone ? styles.withError : ""}>
-          <span>Phone</span>
-          <input type="text" {...register("phone", requiredRegisterArgs)} />
-          {errors.phone && (
-            <span className={styles.error}>{errors.phone.message}</span>
-          )}
-        </label>
-        <Textarea
-          minRows={4}
-          maxRows={4}
-          placeholder="Message..."
-          {...register("message")}
-        />
-      </div>
-      <button type="submit">Send message</button>
-    </form>
+    <>
+      {contextHolder}
+      <form
+        className={`${styles.formWrapper} ${wrapperClassname}`}
+        onSubmit={handleSubmit(onSubmit, onError)}
+      >
+        <div className={styles.formHeader}>
+          <span className={styles.title}>Contact us</span>
+          <span className={styles.secondaryTitle}>
+            Still have more questions? Would be glad to assist.
+          </span>
+        </div>
+        <div className={styles.fields}>
+          <label className={errors.name ? styles.withError : ""}>
+            <span>Name</span>
+            <input type="text" {...register("name", requiredRegisterArgs)} />
+            {errors.name && (
+              <span className={styles.error}>{errors.name.message}</span>
+            )}
+          </label>
+          <label className={errors.email ? styles.withError : ""}>
+            <span>Email</span>
+            <input type="email" {...register("email", requiredRegisterArgs)} />
+            {errors.email && (
+              <span className={styles.error}>{errors.email.message}</span>
+            )}
+          </label>
+          <label className={errors.phone ? styles.withError : ""}>
+            <span>Phone</span>
+            <input type="text" {...register("phone", requiredRegisterArgs)} />
+            {errors.phone && (
+              <span className={styles.error}>{errors.phone.message}</span>
+            )}
+          </label>
+          <Textarea
+            minRows={4}
+            maxRows={4}
+            placeholder="Message..."
+            {...register("message")}
+          />
+        </div>
+        <button type="submit">Send message</button>
+      </form>
+    </>
   );
 };
 
